ab8d6b652b4299eb6bdfd6b49c1cab10aa432816
Prink Hapaliya <90205818+prink22@users.noreply.github.com>
2023-03-18 17:59:51 +0000

    add layout

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__,)
 
-
+a="hello how are you?";
 @app.route("/")
 def hello_world():
-  return "hello"
+  return render_template('home.html',
+                        image_name='good',name=a)
 
 
 if __name__ == '__main__':
